5d02b7ee94dc8247d890eaf48ae23f4481969f88
Asaf erdman <asaferdman23@gmail.com>
2023-07-30 16:11:40 +0300

    Header ui

---
+++ src/app/Theme.js
@@ -0,0 +1,10 @@
+// theme.js
+export const theme = {
+    colors: {
+      blue: "#0075C9",
+      gray: "#5C6873",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+  };
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import Image from 'next/image'
 import DashboardIntilize from './Components/Dashboard/DashboardIntilize'
 import Header from './Components/Header/Header'
 import SideMenu from './Components/SideMenu/SideMenu'
+import { theme } from './Theme'
 
 export default function Home() {
+  const {colors } = theme;
   return (
     <main>
       <head>
           <title>Iot project</title>
         </head>
-      <div>
+      <div style={{ backgroundColor: colors.gray, color: colors.white, padding: "20px" }}>
       <Header/>
       <SideMenu/>
       <DashboardIntilize/>
